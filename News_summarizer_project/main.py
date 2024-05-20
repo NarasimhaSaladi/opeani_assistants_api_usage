23dabf75670f3bbe014e510e0e17fec2df4a6cb1
NarasimhaSaladi <narasimhasaladi7@gmail.com>
2024-05-21 02:22:08 +0530

    added code

--- News_summarizer_project/main.py
+++ News_summarizer_project/main.py
@@ -77,8 +77,89 @@ def get_news(topic):
         print("error occcured during API Request",e)
 
 def main():
-    print(get_news("bitcoin"))
+    # print(get_news("bitcoin"))
+    news2=get_news("bitcoin")
+    print(news2[0])
+    
+    
+    
+    
+class AssistantManager:
+    thread_id=None
+    assistant_id=None
+    
+    def __init__(self,model:str = model)->None:
+        self.client=client
+        self.model=model
+        self.assistant=None
+        self.thread=None
+        self.run=None
+        self.sum=None
+    if AssistantManager.assistant_id:
+        self.assistant = self.client.beta.assistants.retrieve(
+        assistant_id=AssistantManager.assistant_id
+    )
 
+    if AssistantManager.thread_id:
+        self.thread = self.client.beta.threads.retrieve(
+            thread_id=AssistantManager.thread_id
+        )
+    def create_assistant(self,name,instructions,tools):
+        if not self.assistant:
+            assistant_obj=self.client.beta.assistants.create(
+                name=name,
+                instruction=instructions,
+                tools=tools,
+                model=self.model
+            )
+            AssistantManager.assistant_id=assistant_obj.id
+            self.assistant=assistant_obj
+            
+            print(f"ASS_id:{self.assistant.id}")
+
+    def create_thread(self):
+        if not self.thread:
+            thread_obj=self.client.beta.threads.create()
+            AssistantManager.thread_id=thread_obj.id
+            self.thread=thread_obj
+            
+            print(f"thread_id:{self.thread.id}")
+        
+    def add_messsages_to_thread(self,role,content):
+        if self.thread:
+            self.client.beta.threads.messages.create(
+                thread_id=self.thread.id,
+                role=role,
+                content=content
+            )
+    def run_assistant(self,instructions):
+        if self.thread and sel.assistant:
+            self.run = self.client.beta.threads.runs.create(
+            thread_id=self.thread.id,
+            assistant_id=self.assistant.id,
+            instructions=instructions,)
+    def process_mess(self):
+        if self.thread:
+            messages=self.client.beta.threads.messages.list(thread_id=self.thread_id)
+            summary=[]
+            
+            last_mess=messages.data[0]
+            response=last_mess.content[0].text.value
+            role=last_mess.role
+            
+            summary.append(response)
+            
+            self.summary="\n".join(summary)
+            print(f"summary ->>>>>>>>>{role.capitalize()}: ->>>>> {response}")
+            
+            # for msg in messages:
+            #     role=msg.role
+            #     content=msg.content[0].text.value
+            #     print(f"summary ->>>>>>>>>{role.capitalize()}: ->>>>> {content}")
+        
+        return summary
+
+    
 if __name__== "__main__":
     main()
     
